90de75d4532aaf36f381d4ff059db81996707ab5
muvahhid-001 <zelem139@gmail.com>
2025-08-14 05:34:04 +0300

    Done

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,8 +2,13 @@ import { Frame } from "@/pages/Frame/ui/Frame";
 import "@/shared/config/fonts.css";
 
 import "./App.scss";
+import { preloadImages } from "@/utils/preloadImages";
+import { useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    preloadImages();
+  }, []);
   return <Frame />;
 };
 
